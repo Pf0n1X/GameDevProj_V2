856dd6ac58de211d7e6ed96104dd31e79fb2a3a2
Tomer <bboypsyfly@gmail.com>
2021-05-22 18:05:55 +0300

    AI Aiming

--- Source/GameDevProj_V2/ShooterAIController.cpp
+++ Source/GameDevProj_V2/ShooterAIController.cpp
@@ -3,3 +3,12 @@
 
 #include "ShooterAIController.h"
 
+#include "Kismet/GameplayStatics.h"
+
+void AShooterAIController::BeginPlay() 
+{
+    Super:: BeginPlay();
+
+    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+    SetFocus(PlayerPawn);
+}
--- Source/GameDevProj_V2/ShooterAIController.h
+++ Source/GameDevProj_V2/ShooterAIController.h
@@ -13,5 +13,7 @@ UCLASS()
 class GAMEDEVPROJ_V2_API AShooterAIController : public AAIController
 {
 	GENERATED_BODY()
-	
+
+protected:
+	virtual void BeginPlay() override;	
 };
